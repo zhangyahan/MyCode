2dc5826e543c0cfb125deac2f7ed3f3597dcc8a0
tjajia <1791123521@qq.com>
2018-09-20 16:49:34 +0800

    photo

---
+++ crawler/host/host-text_config.py
@@ -0,0 +1,19 @@
+# from host_text import *
+
+
+# herad = {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+#          "Accept-Encoding": "gzip, deflate, br",
+#          "Accept-Language": "zh-CN,zh;q=0.9",
+#          "Cache-Control": "max-age=0",
+#          "Connection": "keep-alive",
+#          "Cookie": "lianjia_uuid=cbbee9ba-67de-4b98-9e6f-f319957733da;"
+#                    "  UM_distinctid=1659fc4b9571d9-02a908a992bb7-3c720356-100200-1659fc4b95e1f0;"
+#                    " select_city=120000; lianjia_ssid=33274adc-410d-4e45-9a56-98fb599009f0;"
+#                    " sajssdk_2015_cross_new_user=1;"
+#                    " sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22165d35ddba210e-0d80d136cddbeb-3c720356-1049088-165d35ddba969%22%2C%22%24device_id%22%3A%22165d35ddba210e-0d80d136cddbeb-3c720356-1049088-165d35ddba969%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E4%BB%98%E8%B4%B9%E5%B9%BF%E5%91%8A%E6%B5%81%E9%87%8F%22%2C%22%24latest_referrer%22%3A%22https%3A%2F%2Fwww.baidu.com%2Fs%22%2C%22%24latest_referrer_host%22%3A%22www.baidu.com%22%2C%22%24latest_search_keyword%22%3A%22%E8%B4%9D%E5%A3%B3%22%2C%22%24latest_utm_source%22%3A%22baidu%22%2C%22%24latest_utm_medium%22%3A%22pinzhuan%22%2C%22%24latest_utm_campaign%22%3A%22sousuo%22%2C%22%24latest_utm_content%22%3A%22biaotimiaoshu%22%2C%22%24latest_utm_term%22%3A%22biaoti%22%7D%7D;"
+#                    " _smt_uid=5b9a763c.18bce04d; www_zufangzi_server=ef9a0d2d79c03c8a1ec4188445a7482d;"
+#                    " CNZZDATA1273627291=1934034889-1535979815-%7C1536846414",
+#                    "Host": host[0],
+#                    # "Referer": "https://"+ host[0] + "/zufang",
+#                    "Upgrade-Insecure-Requests": 1,
+herad = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"}

55ef8c7130ea3c5d5099daa39b1f89c0b81b3215
[zyh] <[1791123521@qq.com]>
2018-09-24 22:02:23 +0800

    IO多路复用

---
+++ MyPythonCode/os.frok父子进程.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import time
+
+
+def inner():
+    time.sleep(3)
+    print("我是耳机紫禁城")
+
+
+def father():
+    time.sleep(1)
+    print("我是父进程")
+
+
+pid = os.fork()
+
+if pid == 0:
+    pid1 = os.fork()
+    if pid1 == 0:
+        inner()
+    else:
+        sys.exit("紫禁城脱出")
+else:
+    father()
+    os.wait()

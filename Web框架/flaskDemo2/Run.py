c12d0c9aca89f7c067e2caaca1202d1c27c92d83
[zyh] <[1791123521@qq.com]>
2018-09-26 23:38:58 +0800

    flask框架练习

---
+++ Web框架/flaskDemo2/Run.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+import pymysql
+pymysql.install_as_MySQLdb()
+
+app = Flask(__name__,template_folder="muban")
+# 指定连接数据库
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:zyh1997@localhost:3306/students"
+# 指定让SQLAlchemy是否跟踪程序的修改
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# 设置自动提交
+app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True
+# 为当前项目创建一个SQLAlchemy实例
+db = SQLAlchemy(app)
+
+
+class student(db.Model):
+    __tablename__ = "students_table"
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(32),nullable=False)
+    age = db.Column(db.SmallInteger)
+    score = db.Column(db.Numeric(5,2))
+
+    def __init__(self,name,age,score):
+        self.name = name
+        self.age = age
+        self.score = score
+
+
+# 将实例映射到数据库
+db.create_all()
+
+# 创建路由
+@app.route("/",methods=["POST","GET"])
+def index():
+    if request.method == "GET":
+        return render_template("insert_student_index.html")
+    else:
+        name = request.form.get("studentName")
+        age = request.form.get("studentAge")
+        score = request.form.get("studentScore")
+
+        student_info = student(name,int(age),float(score))
+        db.session.add(student_info)
+        return "提交成功"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

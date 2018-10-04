5381298ed281743dbb6e94432319e67639e4ea8f
tjajia <1791123521@qq.com>
2018-10-04 14:48:31 +0800

    练习

--- Web框架/flaskDemo1/Run.py
+++ Web框架/flaskDemo1/Run.py
@@ -18,5 +18,17 @@ def index():
         return "提交成功"
 
 
+@app.route("/login",methods=["GET","POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        name = request.form["username"]
+        password = request.form["password"]
+        ispwd = request.form["ispwd"]
+        print(name,password,ispwd)
+        return "登录成功"
+
+
 if __name__ == '__main__':
     app.run(debug=True)

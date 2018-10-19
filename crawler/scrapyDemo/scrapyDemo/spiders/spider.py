1b24603abdbde6a04e8596b7b243c4a15538aa2e
zhangyahan <1791123521@qq.com>
2018-10-19 16:47:30 +0800

    scrapy

---
+++ crawler/scrapyDemo/scrapyDemo/spiders/spider.py
@@ -0,0 +1,7 @@
+import scrapy
+from scrapyDemo.items import ScrapydemoItem
+from scrapy.spiders import Rule, CrawlSpider
+from scrapy.linkextractors import LinkExtractor
+
+class Spider(CrawlSpider):
+    pass

dba65a1805fa8fb6b2b2c3b4cb19b4bf2e604a06
[zyh] <[1791123521@qq.com]>
2018-11-16 23:59:00 +0800

    基础

--- MyPythonCode/迭代器_生成器.py
+++ MyPythonCode/迭代器_生成器.py
@@ -28,3 +28,68 @@
 # print("hello")
 # print("hello")
 
+# 迭代器协议: 对象必须提供一个next方法,执行该方法要么返回迭代中的下一项
+# 要么就引起一个Stoplteraion异常,已终止迭代(只能往后走不能往前退)
+
+# 迭代器对象: 实现了迭代器协议的对象(如何实现:对象内部定义一个__iter__方法)
+
+# 协议是一种约定,可迭代对象实现了迭代器协议, Python的内部工具
+# (如for循环,sum,min,max函数等)使用迭代器协议访问对象
+
+# l = [1, 2, 3]
+# for i in l:
+#     print(i)
+
+
+# x = "hello"
+
+# iter_test = x.__iter__()
+# print(iter_test)
+# try:
+#     print(iter_test.__next__())
+#     print(iter_test.__next__())
+#     print(iter_test.__next__())
+#     print(iter_test.__next__())
+#     print(iter_test.__next__())
+#     print(iter_test.__next__())
+#     print(iter_test.__next__())
+# except StopIteration:
+#     pass
+
+
+# def shengchanbaozi():
+#     for i in range(100):
+#         yield '一屉包子{}'.format(i)
+# 
+# 
+# for i in shengchanbaozi():
+#     print(i)
+
+# def mujixiadan():
+#     ret = []
+#     for i in range(10000):
+#         ret.append("鸡蛋{}".format(i))
+#     return ret
+# 
+# dan = mujixiadan()
+# print(dan)
+
+# 缺点1:占用空间大
+# 缺点2:效率低
+
+def mujixiadan():
+    for i in range(1000):
+        yield '鸡蛋{}'.format(i)
+
+alex_jidan = mujixiadan()
+jidan = alex_jidan.__next__()
+print("鸡蛋%s" % jidan)
+
+# 生成器总结
+# 语法上和函数类似: yield可以保存状态,
+# 优点1:生成器的好处是延迟计算,一次返回一个结果
+# 也就是说,它不会
+
+
+
+

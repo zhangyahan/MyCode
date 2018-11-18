1c963d47f00be89bf200cf00759ca9e1bd8c7a49
[zyh] <[1791123521@qq.com]>
2018-11-18 22:52:22 +0800

    test

---
+++ 练手小项目/选课系统.py
@@ -0,0 +1,26 @@
+
+# 学校类
+class School(object):
+    def __init__(self, name):
+        self.name = name
+        
+        
+
+
+# 学生类
+class Student(object):
+    pass
+
+
+
+# 课程类
+class Class(object):
+    pass
+
+
+
+# 教师类
+class Tecath(object):
+    pass
+
+
